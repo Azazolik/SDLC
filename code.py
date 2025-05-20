3cdb9e1eccf087e429a4ae3033bccce6ae36f436
Azazolik <yarik29021@gmail.com>
2025-05-20 10:16:35 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,5 @@
-
+name = input('Enter your name:')
+age=input('How old are you:')
+print(f'Привіт,{name},тобі зараз {age} років.')
+print('Привіт,{},тобі зараз {} років. '.format(name,age))
+print('Привіт,'+ name + ',тобі зараз '+ age + ' років.')
